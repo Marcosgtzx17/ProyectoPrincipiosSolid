e24008daf918f3a0653445633fe82bedb6f44ba0
Marquinhos <cg25003@esfe.agape.edu.sv>
2025-07-01 00:09:14 -0600

    se cree la clase Client

---
+++ src/main/java/Client.java
@@ -0,0 +1,18 @@
+// Client.java
+public class Client {
+    private String name;
+    private String document;
+
+    public Client(String name, String document) {
+        this.name = name;
+        this.document = document;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+}
